28d013838b435491d7e38efdc2b5bc81443a14a5
Ian McCausland <imccausl@gmail.com>
2017-07-26 10:47:18 -0400

    Changed some variable names for better clarity of purpose

--- dev/js/modules/ListProjects.js
+++ dev/js/modules/ListProjects.js
@@ -1,7 +1,9 @@
-const ListProjects = (query) => {
+const ProjectsList = (query) => {
   const searchQuery = query || "https://api.github.com/search/repositories?q=topic:portfolio+user:imccausl";
-  let searchData = [];
+  let hasData = false;
+  let cache = [];
   let state = [];
+  let view = [];
 
   function getData() {
     return new Promise((resolve, reject) => {
@@ -33,10 +35,73 @@ const ListProjects = (query) => {
     return parsedData;
   }
 
+  function getReadme(url) {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      url = url + "/readme";
+
+      req.onload = resolve(req.responseText);
+      req.onerror = reject(req.statusText);
+      req.open("GET", url, true);
+      req.send();
+    });
+  }
+
+  function makeView(currState) {
+    let view = [];
+    let iterator = 0;
+
+    function parseTitle(title) {
+      let temp = title.split("-");
+      let newTitle = [];
+
+      temp.forEach(word => {
+        word = word.substr(0,1).toUpperCase() + word.substr(1,word.length);
+        newTitle.push(word);
+      });
+
+      return newTitle.join(" ");
+    }
+
+    currState.forEach(item => {
+      let itemView = "";
+
+      itemView = `<div class="portfolio--display text-center col-md-12">
+                    <h4>${parseTitle(item.name)}</h4>
+                    <img src="http://raw.githubusercontent.com/imccausl/${item.name}/master/screenshot.png" width="95%" height="95%">
+                    <p class="portfolio--description text-center">${item.description}</p>
+                    <a class="btn btn-default btn-sm" href="${item.html_url}" role="button">View Source</a> 
+                    <a class="btn btn-default btn-sm ${(item.homepage) ? "" : "disabled"}" href="${item.homepage}" role="button">View Project</a>
+                  </div>`
+
+      view.push(itemView);
+    });
+
+    return view;
+  }
+
+  function searchData(model) {
+
+  }
+
+  function sortData(model) {
+
+  }
+
+  function render(vw, anchor) {
+    let anchorElem = document.querySelector(anchor);
+
+    anchorElem.innerHTML = vw.join("");
+  }
+
   getData().then(response => {
-    state = response;
-    searchData = parseData(state);
-    console.log(searchData);
+    cache = parseData(response);
+    hasData = true;
+    
+    view = makeView(cache);
+    render(view, "#project-view");
+
+    console.log(view);
   }) 
   .catch(err => {
     console.log(err);
@@ -44,5 +109,3 @@ const ListProjects = (query) => {
 
 
 };
-
-export default ListProjects;
