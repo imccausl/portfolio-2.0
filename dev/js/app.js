68fa5a1fc373e7896eb325ba92679c3095d108c1
Ian McCausland <imccausl@gmail.com>
2017-07-29 01:00:13 -0400

    Fix synchronous XMLHttpRequest warnings

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,20 +1,37 @@
+/*
+  TODO: Code splitting to load modules that are dependent on particular views.
+  TODO: Refactor some view rendering code for less repetition.
+  TODO: Eventually create smaller, fixed header/nav menu when scroll exeeds the main header. (not a priority for this version).
+  TODO: Configure webpack for cache-busting 
+  TODO: Finish/clean up about page bio
+  TODO: Add one more colum to about page highlighting OS experience (Linux, Mac, Windows/WSL)?
+  TODO: Deploy to gh-pages!
+*/
 import Router from './modules/Router';
+import CodingTimeWidget from './modules/CodingTimeWidget';
+import ReadingList from './modules/ReadingList';
 import ListProjects from './modules/ListProjects';
+import Fetch from './modules/Fetch';
+
+const key = "ilydZbBaVOPX55J3xKuS1A";
 
 Router.config({mode:'history'});
 
 Router.add(/about/,()=>{
-  console.log("About");
-  $.get('./dev/routes/about.html').done(data => $('#main-view').html(data));
+  $('#main-view').load('dev/routes/about.html', ()=>{
+    CodingTimeWidget($);
+    ReadingList(Fetch, key);
+  })
 })
 
 .add(/projects/, ()=>{
-  console.log("Projects");
-  $.get('./dev/routes/projects.html').done(data => $('#main-view').html(data));
+  $('#main-view').load('dev/routes/projects.html', ()=>{
+    ListProjects();
+  });
 })
 
 .add(/blog/, ()=>{
   console.log("Blog");
 })
 
-.check('/about').listen();
+.check(Router.getFragment()).listen();
