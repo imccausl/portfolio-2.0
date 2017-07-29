f071192641d1a7d924e85d171e9d8926c51ed503
Ian McCausland <imccausl@gmail.com>
2017-07-29 01:00:48 -0400

    Load placeholder data for reading list if ajax call fails

--- dev/js/modules/ReadingList.js
+++ dev/js/modules/ReadingList.js
@@ -6,14 +6,21 @@ const ReadingList = (fetch, key) => {
     const currReadingURL = `https://www.goodreads.com/review/list/15487566.xml?key=${key}&id=15487566-ian-mccausland&v=2&shelf=${shelf}`;
 
     fetch(currReadingURL, "xml", true).then(response =>{
+      console.log(response);
       const book = response.getElementsByTagName("book")[0];
-      const bookTitle = book.childNodes[11].textContent;
-      const bookAuthorNode = book.childNodes[41].innerHTML;
-      const bookGrId = book.childNodes[1].textContent;
-      const bookURL = "https://www.goodreads.com/book/show/" + bookGrId;
-      const bookAuthor = bookAuthorNode.substring(bookAuthorNode.indexOf("<name>") + 6, bookAuthorNode.indexOf("</name>"));
-     
-      render(makeView(bookTitle, bookAuthor, bookURL), "#currently-reading--view");
+      if(book !== undefined) {
+        const bookTitle = book.childNodes[11].textContent;
+        const bookAuthorNode = book.childNodes[41].innerHTML;
+        const bookGrId = book.childNodes[1].textContent;
+        const bookURL = "https://www.goodreads.com/book/show/" + bookGrId;
+        const bookAuthor = bookAuthorNode.substring(bookAuthorNode.indexOf("<name>") + 6, bookAuthorNode.indexOf("</name>"));
+  
+        render(makeView(bookTitle, bookAuthor, bookURL), "#currently-reading--view");
+      } else {
+        render(makeView("Programming JavaScript Applications: Robust Web Architecture With Node, HTML5, and Modern JS Libraries", "Eric Elliott", "https://www.goodreads.com/book/show/15812614"), "#currently-reading--view");
+      }
+    }).fail(err => {
+      render(makeView("Programming JavaScript Applications: Robust Web Architecture With Node, HTML5, and Modern JS Libraries", "Eric Elliott", "https://www.goodreads.com/book/show/15812614"), "#currently-reading--view");
     });
   }
 
@@ -21,8 +28,8 @@ const ReadingList = (fetch, key) => {
     return `I am currently reading <a href="${url}">${title}</a> by <span class="text--salient">${author}</span>.`
   }
 
-  function render(view, elm) {
-    const anchor = document.querySelector(elm);
+  function render(view, elem) {
+    const anchor = document.querySelector(elem);
 
     anchor.innerHTML = view;
   }
