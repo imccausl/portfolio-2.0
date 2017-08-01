05255dfe6305aa5f3ba4a6d1cc4ce059e0c65362
Ian McCausland <imccausl@gmail.com>
2017-07-31 21:53:37 -0400

    Fixed paths in webpack config for new build/deploy tasks

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   context: __dirname + '/dev/js',
+  devtool: 'eval-cheap-module-source-map',
   entry: {
     app: './app.js',
   },
   output: {
-    path: __dirname + '/dist/js',
+    path: __dirname + '/dev/js',
     filename: 'bundle.js',
   },
   module: {
@@ -18,4 +21,11 @@ module.exports = {
       }
     ],
   },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      template: __dirname + '/dev/app.html',
+      filename: __dirname + '/dev/index.html',
+      hash: true,
+    }
+  )],
 };
