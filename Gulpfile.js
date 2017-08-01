c05c20f0b2e270568e6c324e6c3f4b96c0e74277
Ian McCausland <imccausl@gmail.com>
2017-07-31 21:49:29 -0400

    Improve gulpfile for building, serving and deploying to production

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,44 +11,88 @@ const webpack = require('webpack');
 const mywpConfig = require('./webpack.config');
 const concat = require('gulp-concat');
 const ghPages = require('gulp-gh-pages');
+const gulpIf = require('gulp-if');
+const rename = require('gulp-rename');
 
-const SASS_PATH = 'dev/scss/**/app.scss';
+const SASS_PATH = 'dev/scss/app.scss';
 
 gulp.task('sass', ()=>{
   return gulp.src(SASS_PATH)
     .pipe(sass())
-    .pipe(gulp.dest('dev/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(gulp.dest('dev/css/'));
 });
 
 gulp.task('browserSync', ()=>{
   browserSync.init({
     server: {
-      baseDir: './',
-      index: 'dev/index.html'
+      baseDir: './dev',
+      index: 'index.html'
     },
+  });
+});
+
+gulp.task('build:serve', ()=>{
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+      index: 'index.html'
+    }
   });
 });
 
 gulp.task('webpack', ()=>{
   return gulp.src('dev/js/app.js')
     .pipe(gulpWebpack( mywpConfig, webpack ))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dev/js/'));
 });
 
 gulp.task('useref', ()=>{
-  return gulp.src('dev/*.html')
+  return gulp.src('dev/index.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dev/'));
+});
+
+gulp.task('useref:build', ()=>{
+  return gulp.src('dev/index.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('fonts', ()=>{
   return gulp.src('dev/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', ()=>{
+  return gulp.src('dev/img/**/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('files', ()=>{
+  return gulp.src('dev/files/**/*')
+    .pipe(gulp.dest('dist/files'));
+});
+
+gulp.task('moveBundle', ()=>{
+  return gulp.src('dev/js/bundle.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('routes', ()=>{
+  return gulp.src('dev/routes/**/*')
+    .pipe(gulp.dest('dist/routes'));
+});
+
+gulp.task('setToIndex', ()=>{
+  return gulp.src('dist/app.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('removeAppHTML', ()=>{
+  return del('dist/app.html');
 })
 
 gulp.task('clean:dist', ()=>{
@@ -60,28 +104,34 @@ gulp.task('watch', ['webpack', 'browserSync', 'sass'], ()=>{
   gulp.watch('dev/*.html', browserSync.reload);
   gulp.watch(['dev/js/**/*.js', '!dev/js/bundle.js'], ['webpack']);
   gulp.watch('dev/js/bundle.js', browserSync.reload);
+  gulp.watch('dev/**/*', ['useref']);
 });
 
 gulp.task('build', (callback) => {
   runSequence('clean:dist',
-    ['webpack', 'sass', 'useref', 'fonts', 'deploy'],
+    ['webpack', 'sass', 'fonts', 'images', 'routes', 'files', 'setToIndex'], 'useref:build', 'removeAppHTML', 'moveBundle',
     callback
   );
 });
 
+gulp.task('build:run', (callback)=>{
+  runSquence('build', 'build:serve', callback);
+});
+
 gulp.task('default', (callback) => {
-  runSequence(['webpack', 'sass', 'browserSync', 'watch'],
+  runSequence(['webpack', 'sass'], 'useref', 'watch',
     callback
   );
 });
 
 gulp.task('deploy', () => {
+  let timestap = new Date();
+
   return gulp.src('./dist/**/*')
-    .pipe(ghPages([
+    .pipe(ghPages(
       {
         origin: "upstream",
-        branch: "master",
-        message: ""
+        branch: "master"
       }
-    ]));
+    ));
 });
