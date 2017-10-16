ae1e70d6b299da1533e027174a7d2dc0209efc48
Ian McCausland <imccausl@gmail.com>
2017-10-16 19:42:13 -0400

    Fix jarring return to default when scrolling to top of document with full screen expanded state active

--- dev/js/modules/shrinkHeader.js
+++ dev/js/modules/shrinkHeader.js
@@ -5,14 +5,13 @@ function shrinkHeader() {
   const headerContent = document.querySelector('.header--content');
   const navButton = document.querySelector('.nav-button');
   const mainView = document.querySelector('#main-view');
-  const shrinkOn = 450;
+  const shrinkOn = 425;
 
   function handleScroll(event) {
     const distanceY = window.pageYOffset || document.documentElement.scrollTop;
 
-    console.log(distanceY);
-
     if (distanceY > shrinkOn && !mainView.classList.contains('push-down-view')) {
+      hero.classList.add('disable-transition');
       hero.classList.remove('hero--expanded');
       hero.classList.add('hero--bar');
 
@@ -21,7 +20,7 @@ function shrinkHeader() {
 
       headerContent.children[0].classList.remove('text--huge');
       headerContent.children[0].classList.add('text--big');
-      headerContent.children[1].classList.add('hidden-xxs');
+      headerContent.children[1].classList.add('wrap-xxs');
       headerContent.children[2].classList.remove('text-center');
 
       headerContent.children[2].classList.add('nav-button--show');
@@ -48,8 +47,14 @@ function shrinkHeader() {
 
       mainView.classList.add('push-down-view');
 
-      window.scrollTo(0, 460);
-    } else if (distanceY < shrinkOn && mainView.classList.contains('push-down-view')) {
+      window.scrollTo(0, 435);
+    } else if (
+      distanceY < shrinkOn &&
+      mainView.classList.contains('push-down-view') &&
+      !hero.classList.contains('hero--full-screen')
+    ) {
+      // expand header
+      hero.classList.remove('disable-transition');
       mainView.classList.remove('push-down-view');
 
       hero.classList.add('hero--expanded');
@@ -59,7 +64,7 @@ function shrinkHeader() {
       header.children[0].classList.add('container');
       headerContent.children[0].classList.add('text--huge');
       headerContent.children[0].classList.remove('text--big');
-      headerContent.children[1].classList.remove('hidden-xxs');
+      headerContent.children[1].classList.remove('wrap-xxs');
       headerContent.children[2].classList.remove('nav-button--show');
 
       headerContent.children[3].classList.add('text-center');
@@ -88,9 +93,11 @@ function shrinkHeader() {
   function handleNavButtonClick(event) {
     event.preventDefault();
 
+    hero.classList.remove('disable-transition');
     navButton.classList.toggle('open');
     headerContent.children[3].classList.toggle('nav-menu--show');
     hero.classList.toggle('hero--full-screen');
+    handleScroll();
   }
 
   window.addEventListener('scroll', handleScroll, false);
